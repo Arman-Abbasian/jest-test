3d32aecd9bd818d76f3ef16b5c360288b1d820c0
Arman Abbasian <a.abbasian@avaparsi.com>
2024-04-02 09:19:05 +0330

    config the jest test in vite react TS app

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
-// jest.config.js
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-  };
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+      "^.+\\.tsx?$": "ts-jest" 
+  // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+  },
+}
